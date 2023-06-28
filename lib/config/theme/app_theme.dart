a313d0ea4c35c41d2599b815a99b018d7b5018a8
danimtnzz <danielmp76@educastur.es>
2023-06-28 02:03:52 +0200

    First Commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color.fromARGB(255, 255, 0, 0);
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.orange,
+  Colors.yellow,
+  Colors.purple,
+];
+class AppTheme{
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }):assert(selectedColor >=0 && selectedColor <= _colorThemes.length - 1, "Colors must be between 0 and ${_colorThemes.length}");
+
+
+  ThemeData theme(){
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor]
+    );
+  }
+}
---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textControler = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40)
+    );
+    final inputDecoration = InputDecoration(
+        hintText: "End your message with a '?'",
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textControler.value.text;
+            print("button: $textValue");
+            textControler.clear();
+          }, 
+        ),
+      );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textControler,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print("Submit value $value");
+        textControler.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
