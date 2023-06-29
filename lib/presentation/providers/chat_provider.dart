7eff5cee1e4400c8983d36cad81116287241ae40
danimtnzz <danielmp76@educastur.es>
2023-06-30 01:36:48 +0200

    Show & add provider messages, & autoscroll

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,12 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
-  List<Message> message = [
+final ScrollController chatScrollController = ScrollController();
+
+
+  List<Message> messageList = [
     Message(text: "Hola", fromWho: FromWho.me),
     Message(text: "Ya vuelves?", fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async{
-    // TODO: implemet method
+    if(text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+
+
+    notifyListeners();
+    moveScrollToBottom();
   }
+
+Future <void> moveScrollToBottom()async{
+  await Future.delayed(const Duration(milliseconds: 100));
+  chatScrollController.animateTo(chatScrollController.position.maxScrollExtent, duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+}
+
 }
--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+
+
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +25,8 @@ class MessageFieldBox extends StatelessWidget {
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
             final textValue = textControler.value.text;
-            print("button: $textValue");
             textControler.clear();
+            onValue(textValue);
           }, 
         ),
       );
@@ -36,7 +39,7 @@ class MessageFieldBox extends StatelessWidget {
       controller: textControler,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
-        print("Submit value $value");
+        onValue(value);
         textControler.clear();
         focusNode.requestFocus();
       },
